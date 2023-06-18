674573d9e689a70e875e6b8f253ff7b0ec91bddb
Hojoon Lee <leehj.david@gmail.com>
2023-06-18 14:57:46 +0900

    Initial commit

---
+++ src/main/java/club/tennis/tenolportal/TenolPortalApplication.java
@@ -0,0 +1,13 @@
+package club.tennis.tenolportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenolPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenolPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/tennis/tenolportal/TenolPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package club.tennis.tenolportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenolPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
